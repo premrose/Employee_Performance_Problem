96b197a2f9e5dde5df681095124d433219919994
Prem Roshan <premroshanm@gmail.com>
2022-08-27 16:33:15 +0530

    Update Employee_performance_Problem.js

--- Employee_performance_Problem.js
+++ Employee_performance_Problem.js
@@ -1,30 +1,54 @@
-function sample(){
-    var text = '{ "allData" : [' +
-        '{ "Date": "23-02-2015", "ProjectName": "AAA ","Hours": 0.5,"Owner": "sankar","Team": "Design","BillingStatus": "Billable" },' +
-        '{ "Date": "18-02-2015","ProjectName": "AAA ","Hours": 1.2,"Owner": "sdcs","Team": "Design","BillingStatus": "Billable" },' +
-        '{ "Date": "17-02-2015","ProjectName": "BBB","Hours": 0.3,"Owner": "anumothu","Team": "Dev","BillingStatus": "Billable" } ]}'; 
-        let a = [], b=[];
-    var obj = JSON.parse(text);
-    for (let i = 0; i<= text.length; i++) {
-        for (let j = 0; j<= i; j++) {
-            let name = obj.allData[i].Owner;
-            let Project = obj.allData[i].ProjectName;
-            let hours = obj.allData[i].Hours;
-            let team = obj.allData[i].Team;
-            let name1 = obj.allData[j].Owner;
-            let Project1 = obj.allData[j].ProjectName;
-            let hours1 = obj.allData[j].Hours;
-            let team1 = obj.allData[j].Team;
-            if ( hours < hours1 ) {
-                a.push(name);
-                console.log(a);
-            }
-            if(team === team1){
-                b.push(team);
-                console.log(b);
+<html>
+<body>
+    <p onclick="sample()">sample</p><br> 
+</body>
+<script>
+        function sample(){
+
+            // getting err in parsing json, so tried with sample data
+
+            const text = '{ "allData" : [' +
+                '{ "Date": "23-02-2015", "ProjectName": "AAA ","Hours": 0.5,"Owner": "sankar","Team": "Design","BillingStatus": "Billable" },' +
+                '{ "Date": "18-02-2015","ProjectName": "AAA ","Hours": 0.42,"Owner": "sdcs","Team": "Design","BillingStatus": "Billable" },' +
+                '{ "Date": "23-02-2015", "ProjectName": "AAA ","Hours": 0.55,"Owner": "sankar","Team": "Design","BillingStatus": "Billable" },' +
+                '{ "Date": "18-02-2015","ProjectName": "AAA ","Hours": 0.25,"Owner": "sdcs","Team": "Design","BillingStatus": "Billable" },' +
+                '{ "Date": "23-02-2015", "ProjectName": "AAA ","Hours": 0.35,"Owner": "sankar","Team": "Design","BillingStatus": "Billable" },' +
+                '{ "Date": "18-02-2015","ProjectName": "AAA ","Hours": 0.62,"Owner": "sdcs","Team": "Design","BillingStatus": "Billable" },' +
+                '{ "Date": "23-02-2015", "ProjectName": "AAA ","Hours": 0.75,"Owner": "sankar","Team": "Design","BillingStatus": "Billable" },' +
+                '{ "Date": "18-02-2015","ProjectName": "AAA ","Hours": 0.22,"Owner": "sdcs","Team": "Design","BillingStatus": "Billable" },' +
+                '{ "Date": "17-02-2015","ProjectName": "BBB","Hours": 0.23,"Owner": "anumothu","Team": "Dev","BillingStatus": "Billable" } ]}'; 
+            
+            let a = [], b=[];
+            let obj = JSON.parse(text);
+            
+            for (let i = 0; i<= text.length; i++) {
+                for (let j = 0; j<= i; j++) {
+                    let name = obj.allData[i].Owner;
+                    let Project = obj.allData[i].ProjectName;
+                    let hours = obj.allData[i].Hours;
+                    let team = obj.allData[i].Team;
+
+                    let name1 = obj.allData[j].Owner;
+                    let Project1 = obj.allData[j].ProjectName;
+                    let hours1 = obj.allData[j].Hours;
+                    let team1 = obj.allData[j].Team;
+
+
+                    // prints least 5 people who worked least hours
+
+                    if ( hours < hours1 || hours1 < hours ) {
+                        a.push(name);
+                        if(a.length == 5){
+                            console.log(a[0]);
+                            console.log(a[1]);
+                            console.log(a[2]);
+                            console.log(a[3]);
+                            console.log(a[4]);
+                        }
+                    }
+
+                }
             }
         }
-    }
-        
-
-}
+</script>
+</html>
